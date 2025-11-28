cd621ca3cdbfae4e0de0f4592edc4f5518007d57
AdityaTiwari <ta2438549@gmail.com>
2025-11-29 00:46:44 +0530

    Theme

--- Snovy5/client/src/context/ThemeContext.tsx
+++ Snovy5/client/src/context/ThemeContext.tsx
@@ -12,7 +12,7 @@ const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const [theme, setTheme] = useState<Theme>(() => {
     const storedTheme = localStorage.getItem("theme") as Theme | null;
-    return storedTheme || "light";
+    return storedTheme || "dark";
   });
 
   useEffect(() => {
